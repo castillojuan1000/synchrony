4c0632d6345d8ec23d757735225765c86775d92e
Juan Castillo <castillojuan1000@gmail.com>
2023-04-01 23:53:46 -0400

    added delete image service and controller

--- src/main/java/com/castillojuan/synchrony/controller/ImgurController.java
+++ src/main/java/com/castillojuan/synchrony/controller/ImgurController.java
@@ -2,6 +2,7 @@ package com.castillojuan.synchrony.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.castillojuan.synchrony.service.ImgurService;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @RestController
 @RequestMapping("/imgur")
@@ -21,24 +23,35 @@ public class ImgurController {
 
     //upload imgur images 
     @PostMapping("/image")
-    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile image) {
+    public ResponseEntity<JsonNode> uploadImage(@RequestParam("file") MultipartFile image) {
         try {
             byte[] imageData = image.getBytes();
-            String response = imgurService.uploadImage(imageData);
+            JsonNode response = imgurService.uploadImage(imageData);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(e.getMessage());
+            return ResponseEntity.status(500).build();
         }
     }
     
     //get imgur images
     @GetMapping("/account/images")
-    public ResponseEntity<String> getAllAccountImages() {
+    public ResponseEntity<JsonNode> getAllAccountImages() {
         try {
-            String response = imgurService.getAllAccountImages();
+            JsonNode response = imgurService.getAllAccountImages();
             return ResponseEntity.ok(response);
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(e.getMessage());
+            return ResponseEntity.status(500).build();
+        }
+    }
+    
+    //delete image
+    @DeleteMapping("/image/{imageHash}")
+    public ResponseEntity<JsonNode> deleteImage(@PathVariable String imageHash) {
+        try {
+            JsonNode response = imgurService.deleteImage(imageHash);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
         }
     }
 }
--- src/main/java/com/castillojuan/synchrony/service/ImgurService.java
+++ src/main/java/com/castillojuan/synchrony/service/ImgurService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.castillojuan.synchrony.ImgurConfiguration;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -20,9 +22,11 @@ public class ImgurService {
     @Autowired
     private ImgurConfiguration imgurConfig;
     
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    
     
     //upload image method
-    public String uploadImage(byte[] imageData) throws IOException {
+    public JsonNode uploadImage(byte[] imageData) throws IOException {
         OkHttpClient client = new OkHttpClient();
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
@@ -37,12 +41,13 @@ public class ImgurService {
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+        	String responseBody = response.body().string();
+            return objectMapper.readTree(responseBody);
         }
     }
     
     //get account images
-    public String getAllAccountImages() throws IOException {
+    public JsonNode getAllAccountImages() throws IOException {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
@@ -52,7 +57,26 @@ public class ImgurService {
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+        	String responseBody = response.body().string();
+            return objectMapper.readTree(responseBody);
+        }
+    }
+    
+    //delete image
+    public JsonNode deleteImage(String imageHash) throws IOException {
+    	//todo: check if the image exists
+    	
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(imgurConfig.apiUrl + "/image/" + imageHash)
+                .addHeader("Authorization", "Bearer 5edaed792b4f552fb9f4b03356a3e7ef8c3d7337")
+                .delete()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+        	String responseBody = response.body().string();
+            return objectMapper.readTree(responseBody);
         }
     }
     
