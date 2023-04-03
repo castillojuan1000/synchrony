6152c26a2a8b27a4ff34ae25b6ab71e6378ccc64
Juan Castillo <castillojuan1000@gmail.com>
2023-04-02 22:01:47 -0400

    implemented auth for post API

--- src/main/java/com/castillojuan/synchrony/controller/ImgurController.java
+++ src/main/java/com/castillojuan/synchrony/controller/ImgurController.java
@@ -1,18 +1,27 @@
 package com.castillojuan.synchrony.controller;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import com.castillojuan.synchrony.entity.Image;
+import com.castillojuan.synchrony.entity.User;
+import com.castillojuan.synchrony.repository.UserRepository;
 import com.castillojuan.synchrony.service.ImgurService;
+import com.castillojuan.synchrony.utils.DecryptToken;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -22,19 +31,33 @@ public class ImgurController {
 
     @Autowired
     private ImgurService imgurService;
+    @Autowired
+    private UserRepository userRepository;
     
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     //upload imgur images 
     @PostMapping("/image")
-    public ResponseEntity<Image> uploadImage(@RequestParam("image") MultipartFile image) {
-        try {
-            byte[] imageData = image.getBytes();
-            Image response = imgurService.uploadImage(imageData);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(500).build();
-        }
+    public ResponseEntity<?> uploadImage(@RequestHeader("Authorization") String authHeader,@RequestParam("image") MultipartFile image) {
+    	
+    	String token = authHeader.startsWith("Bearer ") ? authHeader.substring(7) : null;
+    	
+    	if(token != null) {
+    		String userName =  DecryptToken.decryptToken(token);
+    		User user = userRepository.findByUsername(userName).orElseThrow(() -> new NoSuchElementException("User not found"));
+    		
+    		try {
+                byte[] imageData = image.getBytes();
+                Image response = imgurService.uploadImage(imageData, user.getId());
+                return ResponseEntity.ok(response);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+    		
+    	}else {
+    		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+    	}
+    	
     }
     
     //get imgur images
