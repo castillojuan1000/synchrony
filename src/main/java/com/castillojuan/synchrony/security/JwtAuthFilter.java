56328c42fcc66db819a1d93dc1b9bda072c52699
Juan Castillo <castillojuan1000@gmail.com>
2023-04-12 20:05:56 -0400

    added jwtAuthFilter layer, enalbles us to get jwt token and perform filters

---
+++ src/main/java/com/castillojuan/synchrony/security/JwtAuthFilter.java
@@ -0,0 +1,35 @@
+package com.castillojuan.synchrony.security;
+
+import java.io.IOException;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter{
+
+	@Override
+	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,@NonNull FilterChain filterChain)throws ServletException, IOException {
+		
+		final String authHeader = request.getHeader("Authorization");
+		final String jwt; 
+		final String userName;
+		
+		if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		jwt = authHeader.substring(7);
+		
+	}
+
+}
